db8bed6a6c68b6b7769ad8cb841ad583e3590df4
Ubuntu <ubuntu@ip-172-31-32-227.sa-east-1.compute.internal>
2023-12-21 14:58:50 +0000

    20231221-1156

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Prior to executing this script:
+# 1. Attach the IAM Role 'S3-Admin-Access' to the Ubuntu EC2 instance
+# 2. Update and upgrade the operating system
+# 3. Install the AWS CLI: sudo apt install -y awscli
+# 4. Reboot the EC2 instance from the AWS Console
+# 5. From the EC2 instance download this script: aws s3 cp s3://gelc/setup.sh .
+
+venv () {
+    sudo apt install -y python3-pip
+    sudo apt install -y python3-venv
+    python3 -m venv my_env
+    . "$HOME"/my_env/bin/activate
+}
+
+clear
+
+venv
+
+cd "$HOME"/my_env || { printf "cd failed, exiting\n" >&2; return 1; }
+git clone https://github.com/laelgelc/tw_aws.git # Update the git repository link accordingly
+cd "$HOME"/my_env/gelc || { printf "cd failed, exiting\n" >&2; return 1; }
+pip install -r env.req # Make sure the file 'env.req' contains the requirements of the environment
+#aws s3 cp s3://gelc/.env .env # Update the reference to the '.env' file accordingly
